29236d33735717f7a3efebf6eeb9d1b23f1a7f27
jerryzhoujw <jerryzhoujw@gmail.com>
2019-08-10 19:13:23 +0700

    include share extension for simulator works

---
+++ ios/ShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ShareExtension
+//
+//  Created by JerryZhou on 2019/8/10.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
